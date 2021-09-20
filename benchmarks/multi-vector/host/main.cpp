9bddac3059f17ff1001e9050ed5dda22380bd608
Arrvindh Shriraman <ashriram@cs.sfu.ca>
2021-09-20 03:04:24 +0000

    Updated multi vector

--- benchmarks/multi-vector/host/main.cpp
+++ benchmarks/multi-vector/host/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include "bench.h"
+#include "../defines.h"
 #include "../../common/m5ops.h"
 
 volatile uint8_t  * top   = (uint8_t  *)0x2f000000;
@@ -12,7 +12,7 @@ volatile uint32_t * val_c = (uint32_t *)0x2f000011;
 int main(void) {
 	m5_reset_stats();
     bool fail = false;
-    stage = 0;
+    //stage = 0;
     uint32_t base = 0x80c00000;
     TYPE *m1 = (TYPE *)base;
     TYPE *m2 = (TYPE *)(base + sizeof(TYPE) * N);
